4331f953ad2999b399e75fa0c076abb373254d7f
evoltech99 <90399602+evoltech99@users.noreply.github.com>
2024-02-03 05:22:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '608f261c-6477-4dd5-9490-6781a55daa81';
-const proxyIPs = ['149.28.62.21'];
+const proxyIPs = ['8.219.238.145'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
