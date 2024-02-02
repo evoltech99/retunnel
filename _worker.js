7716c3d56aa9978dbd17ecc3450fe1391e7a58f0
evoltech99 <90399602+evoltech99@users.noreply.github.com>
2024-02-03 06:37:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = 'http://bgwgmtnp:dhbogrhp9hjq@45.155.68.129:8133';
+let proxyIP = '193.123.231.6';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
